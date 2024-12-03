65fa673bf998f025e820a6037b6e6eba08e399d3
winston <winston.ong92@gmail.com>
2024-12-03 16:17:19 -0500

    Modify SW build: - Add artys7-demo sw build flow

---
+++ sw/c/artys7-demo/main.c
@@ -0,0 +1,18 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdbool.h>
+
+#include "demo_system.h"
+#include "gpio.h"
+#include "pwm.h"
+#include "timer.h"
+
+
+int main(void) {
+
+    // Do nothing
+
+    return 0;
+}
