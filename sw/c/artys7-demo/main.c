97a406ed5ed23e78a803bfd3a786165968145006
winston <winston.ong92@gmail.com>
2024-12-05 11:36:52 -0500

    Some test software to stimulate the peripherals and to view the waveform

--- sw/c/artys7-demo/main.c
+++ sw/c/artys7-demo/main.c
@@ -10,9 +10,26 @@
 #include "timer.h"
 
 
+#define GPIO_BASE_ADDR 0x80000000;
+#define GPIO_END_ADDR 0x80000000 + 4*1024;
+
 int main(void) {
 
-    // Do nothing
+    // Create pointer to Memory location and write to it
+    volatile uint32_t* memptr = 0x44a00000;
+    *memptr = 0xdeadc0de;
+
+    // Writes to GPIO
+    volatile uint32_t* gpioptr = GPIO_BASE_ADDR;
+    *gpioptr = 0xbad0beef; 
+
+    // Exit
+    puts("Hello Opal Test");
+
+    // Delay to allow UART to finish transmitting
+    for(int i=0; i < 10000; i++) {
+        asm volatile("nop");
+    }
 
     return 0;
 }
