d9a25aa62ffa59302e696e7ecc20f02bfdb197a1
winston <winston.ong92@gmail.com>
2025-03-03 11:52:54 -0500

    [SW] Initial Commit for myprintf

--- sw/c/artys7-demo/test_uart.c
+++ sw/c/artys7-demo/test_uart.c
@@ -3,6 +3,27 @@
 #include "gpio.h"
 
 
+int myprintf(char* string) {
+
+    if(string == NULL) {
+        return 1; //ERROR
+    }
+
+    // New pointer value
+    char* ptr = string;
+
+    while(*ptr != '\0') {
+        puts(ptr);
+        ptr++;
+    }
+    return 0;
+}
+
+
 void test_uart(void) {
-    puts("Hello Opal Test\n");
+    // puts("Hello Opal Test\n");
+
+    myprintf("test my print\n");
+    char string_var = "a";
+    myprintf(string_var);
 }
