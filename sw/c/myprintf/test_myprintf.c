e864f0aee36a5cda3f70d10535b4192cfd8a11df
Winston <winston.ong92@gmail.com>
2025-03-13 10:56:52 -0400

    Printing basic integers

--- sw/c/myprintf/test_myprintf.c
+++ sw/c/myprintf/test_myprintf.c
@@ -2,43 +2,48 @@
 #include "demo_system.h"
 #include "gpio.h"
 #include "myprintf.h"
+#include "dev_access.h"
 
+// void test_putchar(void) {
+//     // Print ASCII
+//     putchar(65); // A
+//     putchar(83); // S
+//     putchar(67); // C
+//     putchar(73); // I
+//     putchar(73); // I
+//     puts("\n");
+//     print_ascii('0');
+//     print_ascii('3');
+//     print_ascii('6');
+//     print_ascii('1');
+//     puts("\n");
+//     print_ascii("125");
+//     puts("\n");
+// }
 
-
-void test_putchar(void) {
-    // Print ASCII
-    putchar(65); // A
-    putchar(83); // S
-    putchar(67); // C
-    putchar(73); // I
-    putchar(73); // I
-    puts("\n");
-    convert_to_ascii('0');
-    convert_to_ascii('3');
-    convert_to_ascii('6');
-    convert_to_ascii('1');
-    puts("\n");
-    convert_to_ascii("125");
-    puts("\n");
-}
-
-void test_puthex(void) {
-    puthex(70); // P
-    puthex(75); // U
-    puthex(74); // T
-    puthex(68); // H
-    puthex(65); // E
-    puthex(78); // X
-}
+// void test_puthex(void) {
+//     puthex(70); // P
+//     puthex(75); // U
+//     puthex(74); // T
+//     puthex(68); // H
+//     puthex(65); // E
+//     puthex(78); // X
+// }
 
 void test_myprintf(void) {
-    test_putchar();
-    // test_puthex();
-    puts("\n");
-    myprintf("test my %d print\n");
-    puts("\n");
-    myprintf("%d");
+    // volatile uint32_t* memptr = (volatile uint32_t*)0x44a00000;
+    // *memptr = 0xdeadc0de;
+    // DEV_WRITE(0x80003000,0x0bad);
 
-    // char string_var = "a";
-    // myprintf(string_var);
+    // int int_var = 123;
+    // int int_var2 = 9;
+    // myprintf("%d\n", int_var, NULL);
+    // puts("\n");
+    // myprintf("test my print int:%d, int2:%d \n", int_var, int_var2, NULL);
+    myprintf("%d", 9567982, NULL);
+    myprintf("%d", 54321, NULL);
+    myprintf("%d", 1000, NULL);
+    myprintf("%d", 1024, NULL);
+
+    puts("\nEND\n");
 }
