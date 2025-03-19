a5aeb5fc56d8a079e4c1b8f318b1b8a422ca02e0
Winston <winston.ong92@gmail.com>
2025-03-19 12:13:06 -0400

    Moving myprintf to sw-tests

--- sw/c/artys7-demo/test_uart.c
+++ sw/c/artys7-demo/test_uart.c
@@ -1,29 +1,8 @@
 #include <stddef.h>
 #include "demo_system.h"
 #include "gpio.h"
-
-
-int myprintf(char* string) {
-
-    if(string == NULL) {
-        return 1; //ERROR
-    }
-
-    // New pointer value
-    char* ptr = string;
-
-    while(*ptr != '\0') {
-        puts(ptr);
-        ptr++;
-    }
-    return 0;
-}
-
+#include "myprintf.h"
 
 void test_uart(void) {
-    // puts("Hello Opal Test\n");
-
-    myprintf("test my print\n");
-    char string_var = "a";
-    myprintf(string_var);
+    puts("Hello Opal Test\n");
 }
