3e966a3b0fed1b5c2613781391f25880a17f9c78
Winston <winston.ong92@gmail.com>
2025-05-02 16:36:05 -0400

    Add randomized word read and writes

--- sw/c/hw-tests/ram-128/main.c
+++ sw/c/hw-tests/ram-128/main.c
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-
+#include <stdlib.h>
 #include "demo_system.h"
 #include "dev_access.h"
 #include "myprintf.h"
@@ -16,21 +16,38 @@ extern uint32_t _sdata;
 extern uint32_t _edata;
 extern uint32_t _vectors_start;
 
-int main(void) {
-    puts("---\n");
-    uint32_t status = 0;
+#define RAM_START 0x00100000
 
-    // Write to PWM to pinpoint point in simulation
-    DEV_WRITE(0x80003000, 0xffffffff);
-    DEV_WRITE(0x00113000,0x0badc0de);
 
-    /* for(int i = 0; i < 3; i++) { */
-    /*     uint32_t test_read =  DEV_READ(0x00113000 + i); */
-    /*     myprintf("Test read:0x%x\n", test_read); */
-    /*     puthex(test_read); */
-    /*     puts("\n"); */
-    /* } */
+uint32_t test_mem_addr(uint32_t addr, int32_t value) {
+    // Write to ADDR
+    DEV_WRITE(addr, value);
+    uint32_t read_val = DEV_READ(addr);
+    if(value == read_val) {
+        myprintf("MEM PASS, addr:0x%x\n", addr);
+        return 0;
+    } else {
+        myprintf("MEM FAIL, addr:0x%x\n", addr);
+        return 1;
+    }
+}
 
+uint32_t test_mem_rand(uint32_t addr) {
+    // Write to ADDR
+    uint32_t rand_val = rand();
+    myprintf("rand_val:0x%x\n", rand_val);
+    DEV_WRITE(addr, rand_val);
+    uint32_t read_val = DEV_READ(addr);
+    if(rand_val == read_val) {
+        myprintf("MEM PASS, addr:0x%x\n", addr);
+        return 0;
+    } else {
+        myprintf("MEM FAIL, addr:0x%x\n", addr);
+        return 1;
+    }
+}
+
+void print_symbols(void) {
     // Test HEAP variables
     myprintf("_vectors_start:%x\n", (uintptr_t)&_vectors_start);
     myprintf("_stext:%x\n", (uintptr_t)&_stext);
@@ -40,7 +57,32 @@ int main(void) {
     myprintf("_stack:%x\n",(uintptr_t)&_stack);
     myprintf("_bss_start:%x\n", (uintptr_t)&_bss_start);
     myprintf("_bss_end:%x\n", (uintptr_t)&_bss_end);
+}
 
+
+int main(void) {
+    puts("---\n");
+    uint32_t status = 0;
+
+    // Write to PWM to pinpoint point in simulation
+    DEV_WRITE(0x80003000, 0xffffffff);
+
+    status = test_mem_addr(0x00113000, 0x0badc0de);
+
+    /* for(int i = 0; i < 1024; i ++) {  */
+    /*     status = test_mem_rand(0x00113000 + (i * 1024)); */
+    /*     if(status == 1) break; */
+    /* } */
+    DEV_WRITE(0x80003000, 0x00120000);
+    status = test_mem_rand(0x00120000);
+    DEV_WRITE(0x80003000, 0x00120010);
+    status = test_mem_rand(0x00120010);
+    DEV_WRITE(0x80003000, 0x00130000);
+    status = test_mem_rand(0x00130000);
+    DEV_WRITE(0x80003000, 0x60000000);
+    status = test_mem_rand(0x60000000);
+
+    print_symbols();
 
     while(1) {
         asm volatile("nop");
